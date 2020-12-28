eb0e7c68143a6c157065f107d86878e709105ed6
ryanjon2040 <mindfreak2040@gmail.com>
2020-12-28 17:11:42 +0530

    Refactor Build.cs

--- Source/AgoraViewportClient/AgoraViewportClient.Build.cs
+++ Source/AgoraViewportClient/AgoraViewportClient.Build.cs
@@ -7,8 +7,13 @@ public class AgoraViewportClient : ModuleRules
 	public AgoraViewportClient(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
+#if UE_4_26_OR_LATER
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "DeveloperSettings" });
+#else
+		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
+#endif
+		
 		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Slate", "SlateCore", "RHI" } );
 	}
 }
